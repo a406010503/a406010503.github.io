954415a5669b65a3725dd009d2285bfd5d4cea73
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-05-18 00:46:12 +0800

    Update en.ts

--- .vitepress/locales/en.ts
+++ .vitepress/locales/en.ts
@@ -1,7 +1,29 @@
-// https://vitepress.dev/guide/custom-theme
-import Theme from 'vitepress/theme-without-fonts'
-import './style.css'
+import { defineConfig } from 'vitepress'
 
-export default {
-  ...Theme
+export default defineConfig({
+    lang: 'en-US',
+    description: 'This is Saint Bear's personal website',
+
+    themeConfig: {
+        nav: nav(),
+
+        lastUpdatedText: "Last updated",
+        darkModeSwitchLabel: 'Dark mode',
+        returnToTopLabel: 'Return to top',
+        outline: {
+            label: 'Label'
+        },
+
+        editLink: {
+            pattern: 'https://github.com/HolyBearTW/holybear.me/edit/main/:path',
+            text: 'Edit this page on GitHub'
+        }
+    }
+})
+
+function nav() {
+    return [
+        { text: 'Telegram', link: 'https://t.me/HolyBearTW' },
+        { text: 'Donate', link: 'https://paypal.me/holybear0610' },
+    ]
 }
